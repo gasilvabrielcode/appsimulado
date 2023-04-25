a37ede43e949e7dbf227c4fa224074ac5cd28edf
Gabriel da Silva <91972934+gasilvabrielcode@users.noreply.github.com>
2023-04-25 11:54:30 -0300

    Add files via upload

---
+++ dev/js/questions.js
@@ -0,0 +1,58 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "A sigla SAMU significa:",
+    answer: "Serviço de Atendimento Móvel de Urgência",
+    options: [
+      "Serviço de Atendimento Médico Urbano",
+      "Serviço de Auxílio Médico de Urgência",
+      "Serviço de Auxílio Móvel ao Usuário",
+      "Serviço de Atendimento Móvel de Urgência"
+    ]
+  },
+    {
+    numb: 2,
+    question: "O órgão responsável pela regulamentação dos padrões da qualidade do ar é o:",
+    answer: "CONAMA",
+    options: [
+      "CONAMA",
+      "PROCONVE",
+      "DETRAN",
+      "Ministério da Saúde"
+    ]
+  },
+    {
+    numb: 3,
+    question: "O julgamento dos recursos interpostos pelos infratores compete:",
+    answer: "à JARI",
+    options: [
+      "ao CONTRAN",
+      "ao CETRAN",
+      "ao CONTRANDIFE",
+      "à JARI"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Entre as técnicas de direção defensiva estão agrupados cinco elementos básicos:",
+    answer: "conhecimento, atenção, previsão, habilidade e ação",
+    options: [
+      "conhecimento, atenção, previsão, habilidade e ação",
+      "atenção, habilidade, experiência de 2 anos, segurança e prudência",
+      "habilidade, ação, prudência, conhecimentos técnicos e previsão",
+      "atenção, previsão, habilidade, conhecimentos mecânicos e prudência"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Aos veículos reprovados na inspeção de segurança e na de emissão de gases poluentes e ruídos será aplicada:",
+    answer: "medida administrativa de retenção",
+    options: [
+      "multa",
+      "apreensão do veículo",
+      "recolhimento do veículo",
+      "medida administrativa de retenção"
+    ]
+  },
+];
